44268da8387ff80d9b42a06500db2dc4ece21322
Yuyao Song <48168412+YY-SONG0718@users.noreply.github.com>
2024-01-03 14:50:19 +0000

    various devels

--- genespectrakg/adapters/process_genespectra_input.py
+++ genespectrakg/adapters/process_genespectra_input.py
@@ -17,3 +17,4 @@ def preprocess_genespectra_output(gene_classes):
     results_few = results_new[['gene', 'specificity_category', 'spec_category', 'spec_score', 'dist_category', 'n_exp', 'mean_exp', 'max_exp', 'frac_exp', 'enriched_group_split']]
     results_few.columns = ['external_gene_name', 'specificity_category_type', 'specificity_category', 'specificity_score', 'distribution_catehory', 'n_expressed', 'mean_expression', 'max_expression', 'fraction_expressed', 'cell_type_name']
     return results_few
+
